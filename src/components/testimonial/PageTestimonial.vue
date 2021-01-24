252a765848a51f04218cb02b1a1bc0862bd0cab2
Nishad Islam <nishad.bdg@gmail.com>
2021-01-24 23:20:22 +0600

    about page updated

--- src/components/testimonial/PageTestimonial.vue
+++ src/components/testimonial/PageTestimonial.vue
@@ -86,28 +86,10 @@ export default {
     return {
       settings: {
         arrows: false,
-        dots: true,
+        dots: false,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 667,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              dots: true,
-            },
-          },
-        ],
       },
     };
   },
